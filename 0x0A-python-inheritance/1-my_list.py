5986f7af0f4748fd823c847b719bb994cc7b907b
miheret-eng <mihherettad2007@gmail.com>
2021-10-12 10:59:21 -0700

    2

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""MyList
+
+"""
+
+
+class MyList(list):
+    
+    """Contains list
+
+    """
+     
+    def print_sorted(self):
+        
+        """Prints self in sorted format
+
+        """
+
+        print(sorted(self))
